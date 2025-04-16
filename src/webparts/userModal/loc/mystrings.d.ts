226e39e93349a68e82f89b08feb76ea68d5c4c92
Bokiej <bokiejp1@emea.roche.com>
2025-04-16 23:57:28 +0200

    init

---
+++ src/webparts/userModal/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IUserModalWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'UserModalWebPartStrings' {
+  const strings: IUserModalWebPartStrings;
+  export = strings;
+}
